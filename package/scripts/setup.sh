7e445f18de3af92812a99707a331d85f6e6751ff
Shane Kumpf <sakserv@sakserv.org>
2015-05-16 13:54:42 -0600

    added new script, changed ldif location

--- package/scripts/setup.sh
+++ package/scripts/setup.sh
@@ -1,116 +1,170 @@
 #!/bin/bash
-set -e 
 
-#"hortonworks"
-export LDAP_PASSWORD=$1
-#"admin"
-export LDAP_ADMIN_USER=$2
-#"hortonworks"
-export DOMAIN=$3
-#"~/security-workshops/ldif"
-export LDIFF_DIR=$4
+#
+# Parse the args
+#
+LDAP_PASSWORD="$1"
+LDAP_ADMIN_USER="$2"
+LDAP_DOMAIN="$3"
+LDAP_LDIF_DIR="$4"
+echo -e "\n####  Installing OpenLDAP with the following args:
+	password: $LDAP_PASSWORD
+	admin user: $LDAP_ADMIN_USER
+	ldap domain: $LDAP_DOMAIN
+	ldif dir: $LDAP_LDIF_DIR
+"
 
-echo "Starting script with password: $1 adminuser: $2 domain: $3"
+#
+# Install EPEL repo
+#
+echo -e "\n####  Installing the EPEL repo"
+cd /tmp && wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm && rpm -ivh epel-release-6-8.noarch.rpm
 
-#enabled logging
-if [ ! -d "/var/log/slapd" ]
-then
-	mkdir /var/log/slapd
+
+#
+# Install OpenLDAP
+#
+echo -e "\n####  Installing OpenLDAP"
+yum install -y openldap-servers openldap-clients
+
+
+#
+# Enabling logging
+#
+echo -e "\n####  Enabling OpenLDAP logging"
+if [ ! -d "/var/log/slapd" ]; then
+    mkdir /var/log/slapd
 fi
 chmod 755 /var/log/slapd/
 chown ldap:ldap /var/log/slapd/
 sed -i "/local4.*/d" /etc/rsyslog.conf
 
-#copy paste the next 4 lines together
 cat >> /etc/rsyslog.conf << EOF
 local4.*                        /var/log/slapd/slapd.log
 EOF
 service rsyslog restart
 
-
-if [ ! -e "/etc/openldap/certs/slapd.pem" ]
-then
-	cd /etc/pki/tls/certs
-	echo US > input.txt
-	echo California >> input.txt
-	echo Palo Alto >> input.txt
-	echo Hortonworks >> input.txt
-	echo Sales >> input.txt
-	echo sandbox >> input.txt
-	echo test@test.com >> input.txt
-	echo >> input.txt
-	
-	make slapd.pem < input.txt
-	rm -f input.txt
-	
-	#check the cert
-	#openssl x509 -in slapd.pem -noout -text
-	
-	chmod 640 slapd.pem
-	chown :ldap slapd.pem
-
-	ln -s /etc/pki/tls/certs/slapd.pem /etc/openldap/certs/slapd.pem
-fi
-
-cd /root
-echo $LDAP_PASSWORD > passwd.txt
-chmod 600 passwd.txt
-export HASH=`slappasswd -T passwd.txt`
-rm -f passwd.txt
-
-echo "Setting up slapd.conf"
-/bin/cp -f /usr/share/openldap-servers/slapd.conf.obsolete /etc/openldap/slapd.conf
-/bin/cp -f /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
-
-sed -i "s/my-domain/$DOMAIN/g" /etc/openldap/slapd.conf
-sed -i "s/Manager/$LDAP_ADMIN_USER/g" /etc/openldap/slapd.conf
-sed -i "s#TLSCertificateFile.*#TLSCertificateFile /etc/pki/tls/certs/ca-bundle.crt#g" /etc/openldap/slapd.conf
-sed -i "s#TLSCertificateKeyFile.*#TLSCertificateKeyFile /etc/pki/tls/certs/slapd.pem#g" /etc/openldap/slapd.conf
-sed -i "s#TLSCACertificatePath.*#TLSCACertificateFile /etc/pki/tls/certs/ca-bundle.crt#g" /etc/openldap/slapd.conf
-#sed -i "s:# rootpw.*\{.*:rootpw $HASH:g" /etc/openldap/slapd.conf
-echo "rootpw $HASH" >> /etc/openldap/slapd.conf
-
-sed -i "s#SLAPD_LDAPS.*#SLAPD_LDAPS=yes#g" /etc/sysconfig/ldap
+#
+# Copy DB_CONFIG and fix ownership
+#
+echo -e "\n####  Copying DB_CONFIG and fixing ownership"
+cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
+chown -R ldap:ldap /var/lib/ldap /etc/openldap
 
 
-echo "BASE dc=$DOMAIN,dc=com" >> /etc/openldap/ldap.conf
-echo "URI ldap://localhost"  >> /etc/openldap/ldap.conf
-echo "TLS_REQCERT never" >> /etc/openldap/ldap.conf
-
-echo "Emptying LDAP"
-#rm -rf /etc/openldap/slapd.d/*
-rm -rf /var/lib/ldap/*
-
-#Setup  structure
-echo "Importing base ldif files"
-sed -i "s/dc=hortonworks/dc=$DOMAIN/g" $LDIFF_DIR/*.ldif
-#slapadd -v -n 2 -l $LDIFF_DIR/base.ldif 
-
-
-echo "Importing other ldif files"
-#slapadd -v -n 2 -l $LDIFF_DIR/groups.ldif
-#slapadd -v -n 2 -l $LDIFF_DIR/adminusers.ldif
-
-echo "Testing slapd"
-#slaptest -f /etc/openldap/slapd.conf -F /etc/openldap/slapd.d
-
-echo "Setting chkconfig on for slapd"
-chkconfig --level 235 slapd on
-
-echo "Correcting permissions of /var/lib/ldap"
-chown -R ldap:ldap /var/lib/ldap
-
-echo "Correcting permissions of /etc/openldap/slapd.d"
-chown -R ldap:ldap /etc/openldap/slapd.d
-chmod -R +r /etc/openldap/slapd.d
-
-echo "Starting slapd service"
+#
+# Start slapd
+#
+echo -e "\n####  Starting slapd"
 service slapd start
 
-echo "Updating phpldapadmin files"
-sed -i "s#Deny from all#Allow from all#g" /etc/httpd/conf.d/phpldapadmin.conf
 
-sed -i "s#^\$servers->setValue('login','attr','uid');#//\$servers->setValue('login','attr','uid');#g" /etc/phpldapadmin/config.php
+#
+# Set the domain suffix
+#
+echo -e "\n####  Setting the domain suffix"
+ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<EOF
+dn: olcDatabase={2}bdb,cn=config
+changetype: modify
+replace: olcSuffix
+olcSuffix: $LDAP_DOMAIN
 
-echo "Restarting webserver"
-service httpd restart
+EOF
+
+#
+# Set the admin user
+#
+echo -e "\n####  Setting the admin user"
+ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<EOF
+dn: olcDatabase={2}bdb,cn=config
+changetype: modify
+replace: olcRootDN
+olcRootDN: cn=$LDAP_ADMIN_USER,$LDAP_DOMAIN
+
+EOF
+
+#
+# Set the admin user password
+#
+echo -e "\n####  Setting the admin password"
+ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<EOF
+dn: olcDatabase={2}bdb,cn=config
+changetype: modify
+replace: olcRootPW
+olcRootPW: $LDAP_PASSWORD
+
+EOF
+
+#
+# Set the config obj admin user
+#
+echo -e "\n####  Setting the config obj admin user"
+ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<EOF
+dn: olcDatabase={0}config,cn=config
+changetype: modify
+replace: olcRootDN
+olcRootDN: cn=$LDAP_ADMIN_USER,cn=config
+
+EOF
+
+
+#
+# Set the config obj admin password
+#
+ldapmodify -Q -Y EXTERNAL -H ldapi:/// <<EOF
+dn: olcDatabase={0}config,cn=config
+changetype: modify
+replace: olcRootPW
+olcRootPW: $LDAP_PASSWORD
+
+EOF
+
+
+#
+# Stop slapd
+#
+echo -e "\n####  Stopping slapd"
+service slapd stop
+
+
+#
+# Enable memberof and refint overlays
+#
+echo -e "\n####  Enabling memberof and refint overlays"
+slapadd -v -n 0 -l $LDAP_LDIF_DIR/memberof_config.ldif
+slapadd -v -n 0 -l $LDAP_LDIF_DIR/refint_config.ldif
+
+
+#
+# Fix ownership
+#
+echo -e "\n####  Fixing ownership"
+chown -R ldap:ldap /var/lib/ldap /etc/openldap
+
+
+#
+# Start slapd
+#
+echo -e "\n####  Starting slapd"
+service slapd start
+
+
+#
+# Add the base ou's
+#
+echo -e "\n####  Adding the base OU's"
+ldapadd -D cn=$LDAP_ADMIN_USER,$LDAP_DOMAIN -w $LDAP_PASSWORD -f $LDAP_LDIF_DIR/base.ldif
+
+
+#
+# Add the admin users and groups
+#
+echo -e "\n####  Adding the admin users and groups"
+ldapadd -D cn=$LDAP_ADMIN_USER,$LDAP_DOMAIN -w $LDAP_PASSWORD -f $LDAP_LDIF_DIR/adminusers.ldif
+
+#
+# Add the end users and groups
+#
+echo -e "\n####  Adding the end users and groups"
+ldapadd -D cn=$LDAP_ADMIN_USER,$LDAP_DOMAIN -w $LDAP_PASSWORD -f $LDAP_LDIF_DIR/endusers.ldif
+
