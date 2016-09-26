89da34cc18969330efe129ac2654980349fa241e
Ali Bajwa <abajwa@hortonworks.com>
2016-09-26 13:57:43 -0700

    added import

--- package/scripts/params.py
+++ package/scripts/params.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-#from resource_management.libraries.functions.version import format_hdp_stack_version, compare_versions
 from resource_management import *
+import os
 
 # server configurations
 config = Script.get_config()
